d7c63ba43dc85542d986868b6fcda3d63cd07095
Oliver Zheng <gitcommits@oliverzheng.com>
2013-03-18 22:44:42 -0700

    Adding JSON validation to PSD extract tests.

--- src/extract/test/test_psd.ts
+++ src/extract/test/test_psd.ts
@@ -3,6 +3,7 @@
 import fs = module('fs');
 import testutil = module('../../test-util');
 import psd = module('../psd');
+var Validator = require('jsonschema').Validator;
 
 var fixtures = [
 	'square',
@@ -10,14 +11,23 @@ var fixtures = [
 ];
 
 export function testGetLayers(test) {
+	var schemaPath = testutil.getSchemaPath('layer');
+	var schema = JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
+
+	var validator = new Validator();
+	validator.addSchema(schema);
+
 	fixtures.forEach(function(fixture) {
-		var psdPath = testutil.getCollateralPath(fixture + '.psd');
-		var extractJson = testutil.getCollateralPath(fixture + '.extract.json');
+		var psdPath = testutil.getPsdPath(fixture + '.psd');
+		var expectedPath = testutil.getPsdPath(fixture + '.extract.json');
 
 		var result = psd.getLayers(psdPath);
-		var expected = JSON.parse(fs.readFileSync(extractJson, 'utf8'));
+		var expected = JSON.parse(fs.readFileSync(expectedPath, 'utf8'));
 
 		test.deepEqual(result, expected);
+
+		var validation = validator.validate(result, schema);
+		test.deepEqual([], validation);
 	})
 
 	test.done();
--- src/test-util.ts
+++ src/test-util.ts
@@ -1,5 +1,9 @@
 import path = module('path')
 
-export function getCollateralPath(filename: string): string {
+export function getPsdPath(filename: string): string {
 	return path.resolve(__dirname, '../collateral/psd', filename)
+}
+
+export function getSchemaPath(name: string): string {
+	return path.resolve(__dirname, '../collateral/schema', name + '.json')
 }
